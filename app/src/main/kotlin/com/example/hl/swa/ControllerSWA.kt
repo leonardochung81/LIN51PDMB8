4cee345db9788b5d58afcdff45a724f4f96cd81e
HugoRessurreicao <hugo.ressurreicao@gmail.com>
2016-11-02 22:49:28 +0000

    submit

--- app/src/main/kotlin/com/example/hl/swa/ControllerSWA.kt
+++ app/src/main/kotlin/com/example/hl/swa/ControllerSWA.kt
@@ -3,7 +3,7 @@ package com.example.hl.swa
 import android.support.v7.app.AppCompatActivity
 
 class ControllerSWA : AppCompatActivity() {
-
+    //test
     private val TAG = javaClass.simpleName
 
 
